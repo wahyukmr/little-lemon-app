2a3d987560e1f5a8d9719a9e2f9a4d4f2bfffb85
wahyukmr <maryu9229@gmail.com>
2023-11-03 08:27:45 +0700

    using HTML semantics and adding meta tags and setting up OGP

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="../public/assets/Logo .svg" alt="Logo Little Lemon" />
+      <Nav />
+    </header>
+  );
+}
